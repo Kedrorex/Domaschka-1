b8fc5a42a81a6f6c6e16cc2a0e43d540f8ae2b08
Kedrorex <belousovvk@bk.ru>
2023-02-13 16:01:11 +0300

    added solution of problem #3

---
+++ HJ3/Program.cs
@@ -0,0 +1,19 @@
+﻿ // Задача 6: Напишите программу, которая на вход принимает число и выдаёт, 
+// является ли число чётным (делится ли оно на два без остатка).
+
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int ostatok = number % 2;
+
+if (ostatok == 1)
+{
+    Console.WriteLine("нет");
+} else
+{
+    Console.WriteLine("да");
+}
