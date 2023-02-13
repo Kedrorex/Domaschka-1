bd7ca6ce8e91b5f9730d99ac2285fb4a06b2654b
Kedrorex <belousovvk@bk.ru>
2023-02-13 15:53:52 +0300

    added solution of problem #1

---
+++ HJ2/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 4: Напишите программу, которая принимает на 
+//вход три числа и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.WriteLine("Введите сравниваемые числа: ");
+
+int n = 3;
+int [] number = new int[n];
+int max = number[0];
+
+for (int i = 0; i < n; i++)
+{
+   number[i] = Convert.ToInt32(Console.ReadLine());
+
+   if(number[i] > max) max = number[i];
+};
+
+Console.WriteLine($"{max}");
+
+
+
